b923a02e32e92c3630417dc4c231f06e7b088f73
nan0scho1ar <scorch267@gmail.com>
2021-10-25 23:25:32 +1000

    changes

--- rot13.py
+++ rot13.py
@@ -10,10 +10,11 @@ def r13(char):
 
 # Rot13 alg for both lowercase and uppercase on whole lines
 def rot13(lines):
-    return "".join([r13(char.lower()).upper() if char.isupper() else r13(char) for char in [line for line in lines]])
+    return "".join([r13(char.lower()).upper() if char.isupper() else r13(char) for char in lines])
 
 # Apply Rot13 alg to a file
 with open(sys.argv[1], "r+") as f:
     lines = f.read()
+    print(lines)
     f.seek(0)
     f.write(rot13(lines))
